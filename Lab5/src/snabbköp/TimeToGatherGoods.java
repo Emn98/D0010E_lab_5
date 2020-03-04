e9dc725d2ece1e07e31ba214e7cf956f83172fa4
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-03-04 09:52:59 +0100

    Add files via upload

---
+++ Lab5/src/snabbköp/TimeToGatherGoods.java
@@ -0,0 +1,21 @@
+package snabbköp.state;
+
+import lab5.random.UniformRandomStream;
+
+public class TimeToGatherGoods {
+	
+	private SnabbköpState state;
+	private UniformRandomStream uniRandStream;
+	
+	
+	TimeToGatherGoods(SnabbköpState state, double lower, double upper, long seed){
+		this.state = state;
+		uniRandStream = new UniformRandomStream(lower, upper, seed);
+	}
+	
+	public double calcTimeToGatherGoods() {
+		return state.getCurrentRunTime() + uniRandStream.next(); 
+	}
+	
+
+}
