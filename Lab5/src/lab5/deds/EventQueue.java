ed97f05bea1a58708cf33ecdf700bd4a0d5b1368
lundsak <lundmarkisak@gmail.com>
2020-03-04 09:48:46 +0100

    Update EventQueue.java

--- Lab5/src/lab5/deds/EventQueue.java
+++ Lab5/src/lab5/deds/EventQueue.java
@@ -9,6 +9,13 @@ public class EventQueue {
 		eventQueue = new ArrayList<Event>();
 	}
 
+	/**
+	 * Adds an event to the queue and sort the queue.
+	 * 
+	 * 
+	 * @param e an Event to add to the eventQueue.
+	 * 
+	 */
 	public void addEvent(Event e) {
 		eventQueue.add(e);
 		SortedSequence sortedSequence = new SortedSequence(eventQueue);
@@ -16,11 +23,30 @@ public class EventQueue {
 		eventQueue = sortedSequence.getSortedQueue();
 	}
 
+	/**
+	 * 
+	 * executes the first element in the queue.
+	 * 
+	 */
 	public void run() {
 		eventQueue.get(0).execute();
 		eventQueue.remove(0);
 	}
 
+	/**
+	 * 
+	 * Clears the queue.
+	 * 
+	 */
+	public void clearQueue() {
+		eventQueue.clear();
+	}
+
+	/**
+	 * Checks if the queue contains anything.
+	 * 
+	 * @return true the queue contains anything. false if the queue is empty.
+	 */
 	public boolean hasNext() {
 		if (!eventQueue.isEmpty()) {
 			return true;
