5540623c61ad8b19b91a81cfdbacbca3d7c59930
lundsak <lundmarkisak@gmail.com>
2020-03-04 10:28:07 +0100

    Update SortedSequence.java

--- Lab5/src/lab5/deds/SortedSequence.java
+++ Lab5/src/lab5/deds/SortedSequence.java
@@ -3,17 +3,25 @@ package lab5.deds;
 import java.util.ArrayList;
 
 public class SortedSequence {
-	private ArrayList<Event> tempQueue = new ArrayList<Event>();
+	private ArrayList<Event> tempQueue;
 
 	public SortedSequence(ArrayList<Event> tempQueue) {
 		this.tempQueue = tempQueue;
 	}
-
+	
+	
 	public ArrayList<Event> getSortedQueue() {
 		return tempQueue;
 	}
-
-	public void sort() { // Sort the unsorted list.
+	
+	
+	
+	/**
+	 * 
+	 * Sort the unsorted list.
+	 * 
+	 */
+	public void sort() {
 		for (int i = 1; i < tempQueue.size(); i++) {
 			Event now = tempQueue.get(i);
 			int j = i - 1;
