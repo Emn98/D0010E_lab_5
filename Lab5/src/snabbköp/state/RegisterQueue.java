1c259ac855c266ed66726d8acbd1ef992b787f10
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-03-05 11:45:56 +0100

    Update RegisterQueue.java

--- Lab5/src/snabbköp/state/RegisterQueue.java
+++ Lab5/src/snabbköp/state/RegisterQueue.java
@@ -3,23 +3,51 @@ package snabbköp.state;
 import java.util.NoSuchElementException;
 import java.util.Vector;
 
+import lab5.deds.State;
+import snabbköp.state.*;
+import snabbköp.state.CustomerFactory.Customer;
+
 public class RegisterQueue {
 	
-	private Vector registerQueue = new Vector();
+	private Vector<Customer> registerQueue = new Vector();
+	private State state;
 	
-	public RegisterQueue(){}
 	
-	public Object removeAndGetFirst() {
+	public RegisterQueue(State state){
+		this.state = state;
+	}
+	
+	
+	
+	public void removeAndGetFirst() {
 		if(registerQueue.isEmpty()) {
 			throw new NoSuchElementException();
 		}else {
-			Object customer = registerQueue.elementAt(0); 
+			Customer customer = registerQueue.elementAt(0); 
 			registerQueue.removeElementAt(0);
-			return customer;
+			((SnabbköpState) state).increaseSuccessfullPurchase();
+			((SnabbköpState) state).increaseNrOfFreeRegisters();
 		}
 	}
 	
 	public boolean isEmpty() {
 		return registerQueue.size() == 0; 
+	}
+	
+	public void add(Customer item) {
+		registerQueue.add(item);
+	}	
+	
+	public int getSize() {
+		return registerQueue.size();
+	}
+	
+	public String toString() {
+		String str = "[";
+		for(int i = 0; i < registerQueue.size(); i++) {
+			str += String.valueOf(registerQueue.elementAt(i).getCustomerId());
+		}
+		str += "]";
+		return str;
 	}
 }
