18a4ff1d6634304b5c32255443492fba87dd0b60
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-03-05 11:51:06 +0100

    Update Simulator.java

--- Lab5/src/lab5/deds/Simulator.java
+++ Lab5/src/lab5/deds/Simulator.java
@@ -1,5 +1,33 @@
 package lab5.deds;
 
-public class Simulator {
+import snabbköp.event.StartEvent;
+import snabbköp.event.StopEvent;
 
+public class Simulator {
+	
+	private Event startEvent;
+	private Event stopEvent;
+	private State state;
+	private EventQueue eventQueue;
+	private View view;
+
+	public Simulator(Event startEvent, Event stopEvent, State state, EventQueue eventQueue, View view) {
+		//this.startEvent = startEvent;
+		//this.stopEvent = stopEvent;
+		this.state = state;
+		this.eventQueue = eventQueue;
+		eventQueue.addEvent(new StartEvent(state,eventQueue));
+		
+		eventQueue.addEvent(new StopEvent(state,eventQueue,10));
+		state.addObserver(view);
+		driver();
+	}
+	
+	private void driver() {
+		while(eventQueue.hasNext() && state.getStopValue() == false) {
+			eventQueue.run();
+		}
+	}
 }
+
+
