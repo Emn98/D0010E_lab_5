f4818c0d57c38b67cb2c26d1bc65d9a4fd9328e8
Semakajje <61454048+Semakajje@users.noreply.github.com>
2020-03-10 16:05:51 +0100

    Update RegisterQueue.java

--- lab5/src/lab5/snabbköp/state/RegisterQueue.java
+++ lab5/src/lab5/snabbköp/state/RegisterQueue.java
@@ -3,8 +3,16 @@ package lab5.snabbköp.state;
 import java.util.ArrayList;
 
 import lab5.deds.Event;
-import lab5.snabbköp.event.CustomerDrivenEvents;
-import lab5.snabbköp.state.CustomerFactory.Customer;
+import snabbköp.event.CustomerDrivenEvents;
+import snabbköp.state.CustomerFactory.Customer;
+
+/**
+ * 
+ * Creates a register queue.
+ * 
+ * @author Karl Näslund, Emil Nyberg, Isak Lundmark.
+ *
+ */
 
 public class RegisterQueue {
 
@@ -13,30 +21,47 @@ public class RegisterQueue {
 	public RegisterQueue() {
 	}
 
+	/**
+	 * 
+	 * Adds a customer to the register queue.
+	 * 
+	 * @param e Takes a customer.
+	 */
 	public void add(Customer e) {
 		registerQueue.add(e);
 	}
 
+	/**
+	 * 
+	 * @return Returns a customer and removes him from the queue.
+	 */
 	public Customer removeFirstCustomer() {
 		return registerQueue.remove(0);
 	}
 
+	/**
+	 * 
+	 * @return Returns true or false if the queue is empty.
+	 */
 	public boolean isEmpty() {
 		return registerQueue.size() == 0;
 	}
-
+	
+	/**
+	 * 
+	 * @return Returns the size of the queue.
+	 */
 	public int getSize() {
 		return registerQueue.size();
 	}
-
+	
+	/**
+	 * @return Returns a string displaying the queue.
+	 */
 	public String toString() {
 		String str = "[";
-		for (int i = 0; i < registerQueue.size(); i++) {
-			if (i == 0) {
-				str += String.valueOf(registerQueue.get(i).getCustomerId());
-			} else {
-				str += ", " + String.valueOf(registerQueue.get(i).getCustomerId());
-			}
+		for(int i = 0; i < registerQueue.size(); i++) {
+			str += String.valueOf(registerQueue.get(i).getCustomerId() +", ");
 		}
 		str += "]";
 		return str;
