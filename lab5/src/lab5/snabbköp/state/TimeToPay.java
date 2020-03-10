aab99ea249e3defe62a12720174363872f012a11
Semakajje <61454048+Semakajje@users.noreply.github.com>
2020-03-10 17:10:12 +0100

    Update TimeToPay.java

--- lab5/src/lab5/snabbköp/state/TimeToPay.java
+++ lab5/src/lab5/snabbköp/state/TimeToPay.java
@@ -2,16 +2,36 @@ package lab5.snabbköp.state;
 
 import lab5.random.UniformRandomStream;
 
+/**
+ * 
+ * Creates a time that decides how long it will take to pay.
+ * 
+ * @author Karl Näslund, Emil Nyberg and Isak Lundmark.
+ *
+ */
 public class TimeToPay {
-
+	
 	private SnabbköpState state;
 	private UniformRandomStream uniRandStream;
-
-	TimeToPay(SnabbköpState state, double lower, double upper, long seed) {
+	
+	/**
+	 * 
+	 * Creates the time.
+	 * 
+	 * @param state Takes a state.
+	 * @param lower Takes the lowest time it can take to pay.
+	 * @param upper Takes the longest time it can take to pay.
+	 * @param seed Takes a seed.
+	 */
+	TimeToPay(SnabbköpState state, double lower, double upper, long seed){
 		this.state = state;
 		uniRandStream = new UniformRandomStream(lower, upper, seed);
 	}
-
+	
+	/**
+	 * 
+	 * @return Returns the time that it will take to pay.
+	 */
 	public double calcTimeToPayForGoods() {
 		return state.getCurrentRunTime() + uniRandStream.next();
 	}
