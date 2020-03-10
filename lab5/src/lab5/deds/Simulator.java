a7bab7791fceccb3e6cde7b2418059e3278c4c78
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-03-10 11:51:24 +0100

    Update Simulator.java

--- lab5/src/lab5/deds/Simulator.java
+++ lab5/src/lab5/deds/Simulator.java
@@ -4,26 +4,32 @@ import lab5.snabbköp.event.StartEvent;
 import lab5.snabbköp.event.StopEvent;
 
 public class Simulator {
-	
+
 	private State state;
 	private EventQueue queue;
 	private View view;
-	
-	public Simulator(State state, EventQueue queue, View view) {
+	private StartEvent start;
+	private StopEvent stop;
+
+	public Simulator(State state, EventQueue queue, View view, StartEvent start, StopEvent stop) {
 		this.state = state;
 		this.queue = queue;
 		this.view = view;
-		
-		queue.addEvent(new StartEvent(state,queue));
-        queue.addEvent(new StopEvent(state,queue,999.00));
-        
-        state.addObserver(view);
-        driver();
-    }
+		this.start = start;
+		this.stop = stop;
 
-    private void driver() {
-        while(queue.hasNext() && state.getStopValue() == false) {
-            queue.run();
-        }
-    }		
+		queue.addEvent(start);
+		queue.addEvent(stop);
+		
+		if (view != null) {
+			state.addObserver(view);
+		}
+		driver();
+	}
+
+	private void driver() {
+		while (queue.hasNext() && state.getStopValue() == false) {
+			queue.run();
+		}
+	}
 }
