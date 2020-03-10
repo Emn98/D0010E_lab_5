b95b6b31c4881fc503a40038c4dcae98f3e995df
Semakajje <61454048+Semakajje@users.noreply.github.com>
2020-03-10 16:05:08 +0100

    Update CustomerFactory.java

--- lab5/src/lab5/snabbköp/state/CustomerFactory.java
+++ lab5/src/lab5/snabbköp/state/CustomerFactory.java
@@ -1,27 +1,54 @@
 package lab5.snabbköp.state;
 
+/**
+ * 
+ * Creates a customer.
+ * 
+ * @author Karl Näslund, Emil Nyberg, Isak Lundmark.
+ *
+ */
+
 public class CustomerFactory {
 	
 	private int customerId = 0;
-
-	public CustomerFactory() {}
 	
-	public Customer makeANewCustomer() {
+	CustomerFactory(){}
+	
+	/**
+	 * 
+	 * @return Returns a new customer.
+	 */
+	public Customer makeNewCustomer() {
 		Customer newCustomer = new Customer(customerId);
-		customerId += 1;
+		customerId++;
 		return newCustomer;
 	}
-	
+
+	/**
+	 * 
+	 * @author Karl Näslund, Emil Nyberg, Isak Lundmark.
+	 *
+	 */
 	public class Customer{
 		
-		private int customerId;
+		private int id;
 		
-		private Customer(int customerId) {
-			this.customerId = customerId;
+		/**
+		 * 
+		 * Gives an id to the customer.
+		 * 
+		 * @param id Takes an id for the customer.
+		 */
+		private Customer(int id){
+			this.id = id;
 		}
 		
+		/**
+		 * 
+		 * @return Returns the customer id.
+		 */
 		public int getCustomerId() {
-			return customerId;
+			return id;
 		}
 	}
 }
