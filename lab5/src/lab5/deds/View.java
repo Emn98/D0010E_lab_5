6884ee6b6719cb36d5bf93562f1f120b96f5980d
lundsak <lundmarkisak@gmail.com>
2020-03-10 15:55:17 +0100

    cOMMENTOS

--- lab5/src/lab5/deds/View.java
+++ lab5/src/lab5/deds/View.java
@@ -2,21 +2,41 @@ package lab5.deds;
 
 import java.util.Observable;
 import java.util.Observer;
-
 import lab5.snabbköp.state.SnabbköpState;
 
-public class View implements Observer{
-	
+/**
+ * 
+ * A general class for the view.
+ * 
+ * @author Isak Lundmark, Emil Nyberg and Karl Näslund.
+ *
+ */
+public class View implements Observer {
+
 	private State state;
 
+	/**
+	 * Creates a view for a sate.
+	 * 
+	 * @param state to create a view for.
+	 */
 	public View(State state) {
 		this.state = state;
 	}
-	
+
+	/**
+	 * Creates an update method for inheritance.
+	 * 
+	 */
 	public void update(Observable o, Object arg) {
-		
+
 	}
-	
+
+	/**
+	 * Gets a state
+	 * 
+	 * @return the viewable state.
+	 */
 	public State getState() {
 		return state;
 	}
