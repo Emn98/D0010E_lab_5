a9d366147c09920b075478fd7beab90d15fce017
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-03-10 10:18:11 +0100

    RunSim Finale -No comments-

--- lab5/src/lab5/main/RunSim.java
+++ lab5/src/lab5/main/RunSim.java
@@ -4,14 +4,19 @@ import lab5.deds.EventQueue;
 import lab5.deds.Simulator;
 import lab5.deds.View;
 import lab5.snabbköp.state.SnabbköpState;
+import lab5.snabbköp.view.SnabbköpsView;
 
 public class RunSim {
 
-	public static void main(String[] args) {
-		
-		EventQueue que = new EventQueue();
-        SnabbköpState state = new SnabbköpState(13,3.0,0.35,0.6,0.6,0.9,2,7,08.00);
-        View view = new View(state);
-        new Simulator(state,que,view);
+	public static void main(String[] args) {		
+//	  EventQueue que = new EventQueue();
+//        SnabbköpState state = new SnabbköpState(1234,1.0,2.0,3.0,0.5,1.0,2,5,10.00);
+//        SnabbköpsView view = new SnabbköpsView(state);
+//        new Simulator(state,que,view);  
+        
+	  EventQueue que = new EventQueue();
+          SnabbköpState state = new SnabbköpState(13,3.0,0.35,0.6,0.6,0.9,2,7,8.00);
+          SnabbköpsView view = new SnabbköpsView(state);
+          new Simulator(state,que,view);  
 	}
 }
