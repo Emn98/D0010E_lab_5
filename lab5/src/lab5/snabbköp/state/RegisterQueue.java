ae02bd1c6c471c1daaf794f63e6d6b0ad40ac441
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-03-10 22:59:33 +0100

    Fixed outdated registerQueue
    Såg att det var en outdated RegisterQueue.

--- lab5/src/lab5/snabbköp/state/RegisterQueue.java
+++ lab5/src/lab5/snabbköp/state/RegisterQueue.java
@@ -3,17 +3,17 @@ package lab5.snabbköp.state;
 import java.util.ArrayList;
 
 import lab5.deds.Event;
-import snabbköp.event.CustomerDrivenEvents;
-import snabbköp.state.CustomerFactory.Customer;
+import lab5.snabbköp.event.CustomerDrivenEvents;
+import lab5.snabbköp.state.CustomerFactory.Customer;
 
 /**
  * 
- * Creates a register queue.
+ * A class to represent an register queue.
  * 
- * @author Karl Näslund, Emil Nyberg, Isak Lundmark.
+ * @author Isak Lundmark, Emil Nyberg and Karl Näslund.
+ * 
  *
  */
-
 public class RegisterQueue {
 
 	private ArrayList<Customer> registerQueue = new ArrayList();
@@ -22,46 +22,54 @@ public class RegisterQueue {
 	}
 
 	/**
+	 * Adds a customer to the back of the queue.
 	 * 
-	 * Adds a customer to the register queue.
-	 * 
-	 * @param e Takes a customer.
+	 * @param e, the customer that will be added to the queue.
 	 */
 	public void add(Customer e) {
 		registerQueue.add(e);
 	}
 
 	/**
+	 * Removes the customer at the front of the queue.
 	 * 
-	 * @return Returns a customer and removes him from the queue.
+	 * @return the customer at the front of the queue.
 	 */
 	public Customer removeFirstCustomer() {
 		return registerQueue.remove(0);
 	}
 
 	/**
+	 * Checks if the queue is empty.
 	 * 
-	 * @return Returns true or false if the queue is empty.
+	 * @return true if the queue is empty, otherwise false.
 	 */
 	public boolean isEmpty() {
 		return registerQueue.size() == 0;
 	}
-	
+
 	/**
+	 * Returns the amount of people standing in queue.
 	 * 
-	 * @return Returns the size of the queue.
+	 * @return the current size of the queue.
 	 */
 	public int getSize() {
 		return registerQueue.size();
 	}
-	
+
 	/**
-	 * @return Returns a string displaying the queue.
+	 * Converts the queue to a string.
+	 * 
+	 * @return A string visualization of the queue.
 	 */
 	public String toString() {
 		String str = "[";
-		for(int i = 0; i < registerQueue.size(); i++) {
-			str += String.valueOf(registerQueue.get(i).getCustomerId() +", ");
+		for (int i = 0; i < registerQueue.size(); i++) {
+			if (i == 0) {
+				str += String.valueOf(registerQueue.get(i).getCustomerId());
+			} else {
+				str += ", " + String.valueOf(registerQueue.get(i).getCustomerId());
+			}
 		}
 		str += "]";
 		return str;
