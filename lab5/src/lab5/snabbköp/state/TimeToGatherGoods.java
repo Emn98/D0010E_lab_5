6174ed838ca7487acd6c6c7c95ba008eb20fc571
Semakajje <61454048+Semakajje@users.noreply.github.com>
2020-03-10 17:06:32 +0100

    Update TimeToGatherGoods.java

--- lab5/src/lab5/snabbköp/state/TimeToGatherGoods.java
+++ lab5/src/lab5/snabbköp/state/TimeToGatherGoods.java
@@ -2,18 +2,38 @@ package lab5.snabbköp.state;
 
 import lab5.random.UniformRandomStream;
 
+/**
+ * Creates a time for gather goods.
+ * 
+ * @author Karl Näslund, Emil Nyberg and Isak Lundmark.
+ *
+ */
 public class TimeToGatherGoods {
 	
 	private SnabbköpState state;
 	private UniformRandomStream uniRandStream;
 	
-	
+	/**
+	 * 
+	 * Creates the time that it will take to gather goods.
+	 * 
+	 * @param state Takes a state.
+	 * @param lower Takes the lowes time to gather goods.
+	 * @param upper Takes the higest time to gather goods.
+	 * @param seed Takes a seed.
+	 */
 	TimeToGatherGoods(SnabbköpState state, double lower, double upper, long seed){
 		this.state = state;
 		uniRandStream = new UniformRandomStream(lower, upper, seed);
 	}
 	
+	/**
+	 * 
+	 * @return Returns the time it will take to gather goods.
+	 */
 	public double calcTimeToGatherGoods() {
 		return state.getCurrentRunTime() + uniRandStream.next(); 
 	}
+	
+
 }
