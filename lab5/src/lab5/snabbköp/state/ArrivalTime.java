179654f738af33e3f4509c3d36003ed6991cf861
Semakajje <61454048+Semakajje@users.noreply.github.com>
2020-03-10 16:04:18 +0100

    Update ArrivalTime.java

--- lab5/src/lab5/snabbköp/state/ArrivalTime.java
+++ lab5/src/lab5/snabbköp/state/ArrivalTime.java
@@ -2,17 +2,36 @@ package lab5.snabbköp.state;
 
 import lab5.random.ExponentialRandomStream;
 
+/**
+ *
+ * Sets the time for a customer arrivalevent.
+ * 
+ * @author Karl Näslund, EmilNyberg and Isak Lundmark
+ *
+ */
 public class ArrivalTime {
-
+	
 	private SnabbköpState state;
 	private ExponentialRandomStream expRandStream;
 
+	/**
+	 * 
+	 * @param state Takes a snabbköpstate.
+	 * @param lambda Takes a double that desides the arrivalspeed.
+	 * @param seed Takes a seed.
+	 */
+	
 	public ArrivalTime(SnabbköpState state, double lambda, long seed) {
 		this.state = state;
 		expRandStream = new ExponentialRandomStream(lambda, seed);
 	}
-
+	
+	/**
+	 * 
+	 * @return Returns the time for when the next customer is to arrive.
+	 */
+	
 	public double calcNextCustArrival() {
 		return state.getCurrentRunTime() + expRandStream.next();
-	}
+	}	
 }
