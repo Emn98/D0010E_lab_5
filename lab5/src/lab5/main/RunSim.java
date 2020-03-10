ff466d7bd0bda7b454d1c4c3af002f2f6d80ccd1
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-03-10 11:57:37 +0100

    Update RunSim.java

--- lab5/src/lab5/main/RunSim.java
+++ lab5/src/lab5/main/RunSim.java
@@ -3,20 +3,24 @@ package lab5.main;
 import lab5.deds.EventQueue;
 import lab5.deds.Simulator;
 import lab5.deds.View;
+import lab5.snabbköp.event.StartEvent;
+import lab5.snabbköp.event.StopEvent;
 import lab5.snabbköp.state.SnabbköpState;
 import lab5.snabbköp.view.SnabbköpsView;
 
 public class RunSim {
 
-	public static void main(String[] args) {		
-//	  EventQueue que = new EventQueue();
+	public static void main(String[] args) {
+//		EventQueue que = new EventQueue();
 //        SnabbköpState state = new SnabbköpState(1234,1.0,2.0,3.0,0.5,1.0,2,5,10.00);
 //        SnabbköpsView view = new SnabbköpsView(state);
 //        new Simulator(state,que,view);  
-        
-	  EventQueue que = new EventQueue();
-          SnabbköpState state = new SnabbköpState(13,3.0,0.35,0.6,0.6,0.9,2,7,8.00);
-          SnabbköpsView view = new SnabbköpsView(state);
-          new Simulator(state,que,view);  
+
+		EventQueue que = new EventQueue();
+		SnabbköpState state = new SnabbköpState(13, 3.0, 0.35, 0.6, 0.6, 0.9, 2, 7, 8.00);
+		SnabbköpsView view = new SnabbköpsView(state);
+		StartEvent start = new StartEvent(state, que);
+		StopEvent stop = new StopEvent(state, que, 999.00);
+		new Simulator(state, que, view, start, stop);
 	}
 }
