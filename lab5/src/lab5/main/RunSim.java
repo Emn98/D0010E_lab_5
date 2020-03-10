8e363349c3410dc3d9f7ba2d00e8dc3784a1b077
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-03-10 22:23:08 +0100

    Nu med båda simmuleringsexemplena ready to go

--- lab5/src/lab5/main/RunSim.java
+++ lab5/src/lab5/main/RunSim.java
@@ -15,9 +15,24 @@ import lab5.snabbköp.view.SnabbköpsView;
  *
  */
 public class RunSim {
+//	//Simuleringsexempel 1
+//	public static final int M = 5;
+//	public static final double L = 1;
+//	public static final int N = 2;
+//
+//	public static final double LOW_COLLECTION_TIME = 0.5d;
+//	public static final double HIGH_COLLECTION_TIME = 1.0d;
+//
+//	public static final double LOW_PAYMENT_TIME = 2.0d;
+//	public static final double HIGH_PAYMENT_TIME = 3.0d;
+//
+//	public static final long SEED = 1234;
+//	public static final double END_TIME = 10.0d;
+//	public static final double STOP_TIME = 999.0d;
 
-	public static final int M = 5;
-	public static final double L = 1;
+	// SimuleringsExempel 2
+	public static final int M = 7;
+	public static final double L = 3.0;
 	public static final int N = 2;
 
 	public static final double LOW_COLLECTION_TIME = 0.6d;
@@ -26,8 +41,8 @@ public class RunSim {
 	public static final double LOW_PAYMENT_TIME = 0.35d;
 	public static final double HIGH_PAYMENT_TIME = 0.6d;
 
-	public static final int SEED = 13;
-	public static final double END_TIME = 8.0d;
+	public static final long SEED = 13;
+	public static final double END_TIME = 08.00d;
 	public static final double STOP_TIME = 999.0d;
 
 	/**
@@ -43,6 +58,6 @@ public class RunSim {
 		SnabbköpsView view = new SnabbköpsView(state);
 		StartEvent start = new StartEvent(state, que);
 		StopEvent stop = new StopEvent(state, que, STOP_TIME);
-		new Simulator(state, que, null, start, stop);
+		new Simulator(state, que, view, start, stop);
 	}
 }
