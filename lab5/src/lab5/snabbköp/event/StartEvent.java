f573f9263a82e59446d3592b01db1c5684e815eb
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-03-10 21:57:57 +0100

    C000MM3nts

--- lab5/src/lab5/snabbköp/event/StartEvent.java
+++ lab5/src/lab5/snabbköp/event/StartEvent.java
@@ -2,17 +2,37 @@ package lab5.snabbköp.event;
 
 import lab5.deds.Event;
 import lab5.deds.EventQueue;
+import lab5.deds.GeneralStartEvent;
 import lab5.deds.State;
 import lab5.snabbköp.state.CustomerFactory.Customer;
 import lab5.snabbköp.state.SnabbköpState;
 
-public class StartEvent extends Event {
+/**
+ * 
+ * A class for the specialized start event.
+ * 
+ * @author Isak Lundmark, Emil Nyberg and Karl Näslund.
+ * 
+ */
+public class StartEvent extends GeneralStartEvent {
 
+	/**
+	 * 
+	 * Constructor. Saves the correct state and queue. .
+	 * 
+	 * @param state that the event can change.
+	 * @param queue that the event will be added to.
+	 */
 	public StartEvent(State state, EventQueue queue) {
 		super(state, queue);
-		this.setExecutionTime(0.000d);
 	}
 
+	/**
+	 * 
+	 * Creates a new customer arrival event and a new closing event and adds them to
+	 * the event queue.
+	 * 
+	 */
 	public void execute() {
 		SnabbköpState state = (SnabbköpState) this.getState();
 		EventQueue queue = this.getQueue();
@@ -23,6 +43,12 @@ public class StartEvent extends Event {
 		queue.addEvent(new ClosingEvent(state, queue));
 	}
 
+	/**
+	 * 
+	 * Returns the name of the event.
+	 * 
+	 * @return The name of the event.
+	 */
 	public String getEventName() {
 		return "Start";
 	}
