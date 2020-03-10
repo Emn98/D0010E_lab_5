0beb7bad42de621656ec2f67dc564793c797b8eb
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-03-10 21:54:03 +0100

    Now with comments

--- lab5/src/lab5/snabbköp/event/CustomerDrivenEvents.java
+++ lab5/src/lab5/snabbköp/event/CustomerDrivenEvents.java
@@ -5,19 +5,45 @@ import lab5.deds.EventQueue;
 import lab5.deds.State;
 import lab5.snabbköp.state.CustomerFactory.Customer;
 
+/**
+ * 
+ * A more general class that will be inherited to every event that is driven by
+ * an customers action.
+ * 
+ * @author Isak Lundmark, Emil Nyberg and Karl Näslund.
+ * 
+ */
 public class CustomerDrivenEvents extends Event {
-	
+
 	private Customer customer;
 
+	/**
+	 * Constructor. Saves the state,queue and the customer to the event.
+	 * 
+	 * @param state    that the event can change.
+	 * @param queue    that the event can add itself and other events to.
+	 * @param customer that will get attached to the event.
+	 */
 	public CustomerDrivenEvents(State state, EventQueue queue, Customer customer) {
 		super(state, queue);
 		this.customer = customer;
 	}
-	
+
+	/**
+	 * 
+	 * Gets the id of the customer that is attached to this event.
+	 * 
+	 * @return an customers id.
+	 */
 	public int getCustomerid() {
 		return customer.getCustomerId();
 	}
-	
+
+	/**
+	 * Gets the customer that is attached to this event.
+	 * 
+	 * @return an customer.
+	 */
 	public Customer getCustomer() {
 		return customer;
 	}
