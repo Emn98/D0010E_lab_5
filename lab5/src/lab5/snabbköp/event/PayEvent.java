069de35e18114cf283c1e07773a1450f06348fa9
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-03-10 21:55:22 +0100

    CoMmEnTs

--- lab5/src/lab5/snabbköp/event/PayEvent.java
+++ lab5/src/lab5/snabbköp/event/PayEvent.java
@@ -5,8 +5,25 @@ import lab5.deds.State;
 import lab5.snabbköp.state.CustomerFactory.Customer;
 import lab5.snabbköp.state.SnabbköpState;
 
+/**
+ * 
+ * A class for pay events.
+ * 
+ * @author Isak Lundmark, Emil Nyberg and Karl Näslund.
+ * 
+ */
 public class PayEvent extends CustomerDrivenEvents {
 
+	/**
+	 * Constructor. Saves the correct state, queue and customer to the event, and
+	 * then calculates the time it takes for the customer to pay for the goods
+	 * before it adds itself to the event queue. It also decreases the number of
+	 * free registers.
+	 * 
+	 * @param state    that the event can change.
+	 * @param queue    that the event will add itself and other events to.
+	 * @param customer the customer that is tied to this event.
+	 */
 	public PayEvent(State state, EventQueue queue, Customer customer) {
 		super(state, queue, customer);
 		double timeToPay = ((SnabbköpState) state).calcTimeToPay();
@@ -14,6 +31,14 @@ public class PayEvent extends CustomerDrivenEvents {
 		((SnabbköpState) state).decreaseNrOfFreeRegisters();
 	}
 
+	/**
+	 * Updates the affected times and the total running time in state. It increases
+	 * the number of available registers, decreases the number of customers
+	 * currently in the store and adds one new successful purchase. If Someone or a
+	 * few people is standing in line to the registers. The first customer in the
+	 * line will be added to a new pay event.
+	 * 
+	 */
 	public void execute() {
 		EventQueue queue = super.getQueue();
 		SnabbköpState state = (SnabbköpState) super.getState();
@@ -35,6 +60,12 @@ public class PayEvent extends CustomerDrivenEvents {
 		}
 	}
 
+	/**
+	 * 
+	 * Returns the name of the event.
+	 * 
+	 * @return The name of the event.
+	 */
 	public String getEventName() {
 		return "Betalning";
 	}
