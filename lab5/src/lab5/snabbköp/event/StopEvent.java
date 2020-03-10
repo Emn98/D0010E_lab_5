65f07d5361bfc1babea4b1f5d8002e1c10f71edb
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-03-10 21:59:29 +0100

    :D:D:DD::D:D:D:D:D:D:

--- lab5/src/lab5/snabbköp/event/StopEvent.java
+++ lab5/src/lab5/snabbköp/event/StopEvent.java
@@ -2,22 +2,45 @@ package lab5.snabbköp.event;
 
 import lab5.deds.Event;
 import lab5.deds.EventQueue;
+import lab5.deds.GeneralStopEvent;
 import lab5.deds.State;
 
-public class StopEvent extends Event {
+/**
+ * 
+ * A class for the specialized stop event.
+ * 
+ * @author Isak Lundmark, Emil Nyberg and Karl Näslund.
+ * 
+ */
+public class StopEvent extends GeneralStopEvent {
 
+	/**
+	 * Constructor. Saves the correct state, queue and stoptime.
+	 * 
+	 * @param state    that the event can change.
+	 * @param queue    that the event can add itself to and other events.
+	 * @param stopTime the time the event will halt.
+	 */
 	public StopEvent(State state, EventQueue queue, double stopTime) {
-		super(state, queue);
-		this.setExecutionTime(stopTime);
+		super(state, queue, stopTime);
 	}
 
+	/**
+	 * Updates the total run time and the halts the simulation.
+	 */
 	public void execute() {
-		State state = this.getState();
+		State state = super.getState();
 		state.updateTotalRunTime(this.getTime());
 		state.notifyObs(this);
 		state.haltSimulation();
 	}
 
+	/**
+	 * 
+	 * Returns the name of the event.
+	 * 
+	 * @return The name of the event.
+	 */
 	public String getEventName() {
 		return "Stop    ";
 	}
