f9dff99ae7889532dff23fc70039b750de7b8791
lundsak <lundmarkisak@gmail.com>
2020-03-10 17:02:50 +0100

    Commmemememttmottom

--- lab5/src/lab5/main/RunSim.java
+++ lab5/src/lab5/main/RunSim.java
@@ -2,25 +2,47 @@ package lab5.main;
 
 import lab5.deds.EventQueue;
 import lab5.deds.Simulator;
-import lab5.deds.View;
 import lab5.snabbköp.event.StartEvent;
 import lab5.snabbköp.event.StopEvent;
 import lab5.snabbköp.state.SnabbköpState;
 import lab5.snabbköp.view.SnabbköpsView;
 
+/**
+ * 
+ * Runs the simulator.
+ * 
+ * @author Isak Lundmark, Emil Nyberg and Karl Näslund.
+ *
+ */
 public class RunSim {
 
-	public static void main(String[] args) {
-//		EventQueue que = new EventQueue();
-//        SnabbköpState state = new SnabbköpState(1234,1.0,2.0,3.0,0.5,1.0,2,5,10.00);
-//        SnabbköpsView view = new SnabbköpsView(state);
-//        new Simulator(state,que,view);  
+	public static final int M = 5;
+	public static final double L = 1;
+	public static final int N = 2;
 
+	public static final double LOW_COLLECTION_TIME = 0.6d;
+	public static final double HIGH_COLLECTION_TIME = 0.9d;
+
+	public static final double LOW_PAYMENT_TIME = 0.35d;
+	public static final double HIGH_PAYMENT_TIME = 0.6d;
+
+	public static final int SEED = 13;
+	public static final double END_TIME = 8.0d;
+	public static final double STOP_TIME = 999.0d;
+
+	/**
+	 * 
+	 * Runs the simulator.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
 		EventQueue que = new EventQueue();
-		SnabbköpState state = new SnabbköpState(13, 3.0, 0.35, 0.6, 0.6, 0.9, 2, 7, 8.00);
+		SnabbköpState state = new SnabbköpState(SEED, L, LOW_PAYMENT_TIME, HIGH_PAYMENT_TIME, LOW_COLLECTION_TIME,
+				HIGH_COLLECTION_TIME, N, M, END_TIME);
 		SnabbköpsView view = new SnabbköpsView(state);
 		StartEvent start = new StartEvent(state, que);
-		StopEvent stop = new StopEvent(state, que, 999.00);
-		new Simulator(state, que, view, start, stop);
+		StopEvent stop = new StopEvent(state, que, STOP_TIME);
+		new Simulator(state, que, null, start, stop);
 	}
 }
